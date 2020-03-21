e3667674a0bcac6c993170c853b087f202204dec
Jost Alemann <jost_alemann@me.com>
2020-03-21 01:43:31 +0100

    crashkurs: components and events

---
+++ lib/components/popup.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Popup extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 56,
+      width: 81,
+      color: Colors.greenAccent,
+      child: Icon(Icons.delete),
+    );
+  }
+}
--- lib/routes/home.dart
+++ lib/routes/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:w27/components/popup.dart';
 import 'package:w27/language/language.dart';
 
 class Home extends StatefulWidget {
@@ -14,9 +15,43 @@ class _HomeState extends State<Home> {
         backgroundColor: Colors.green,
         title: Text('W27'),
       ),
-      body: Center(
-        child: Text(t('hello')),
-      ),
+      body: Stack(
+        children: [
+          Container(
+            color: Colors.amber,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Text(t('hello')),
+                Icon(Icons.access_alarms, size: 100)
+              ],
+            ),
+          ),
+          Positioned(
+            bottom: 10,
+            right: 10,
+            child: Container(
+              color: Colors.blue,
+              width: 100,
+              height: 100,
+              child: Center(child: Text('Deine Mudda'))
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: Container(
+              width: 200,
+              child: RaisedButton(
+                onPressed: () {
+                  print('Hello World');
+                },
+                child: Icon(Icons.hdr_on),
+              )
+            ),
+          )
+        ],
+      )
     );
   }
 }
